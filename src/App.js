1b3cfde6cf3f2917bbd425eeed8911469c1ec61d
simgish <ryandoughty@gmail.com>
2021-07-10 21:53:33 -0400

    retrieving test entries from firebase storage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import EntryForm from './components/EntryForm';
 import EntryList from './components/EntryList';
 import { useState, useEffect } from 'react';
 import entryService from './services/EntryService';
+import db from './firebase.config';
 
 function App() {
   const [entries, setEntries] = useState([]);
   const [categories, setCategories] = useState(['Bedtime', 'Schedule', 'Schoolwork']);
 
   useEffect(() => {
-    entryService.getAll().then(entries => {
-      console.log(entries.data);
-      setEntries(entries.data.data);
-    });
+    // entryService.getAll().then(entries => {
+    //   console.log(entries.data);
+    //   setEntries(entries.data.data);
+    // });
+
+    db.collection("entries")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        console.log('entries: ', data);
+
+        setEntries(data);
+      });
 
   }, [])
 
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,8 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase";
 
-var firebaseConfig = {
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
+const firebaseConfig = {
   apiKey: "AIzaSyDfsEpeVt894FwqINTwlbYHwL0ohr4ohUs",
   authDomain: "rodoc-e2871.firebaseapp.com",
   projectId: "rodoc-e2871",
@@ -10,8 +18,10 @@ var firebaseConfig = {
   messagingSenderId: "214485246603",
   appId: "1:214485246603:web:c054af33b19f5d0daeaa8e"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.firestore().settings({ timestampsInSnapshots: true });
 
-export default firebase;
+console.log('firebaseConfig: ', firebaseConfig);
+
+const initFirebase = firebase.initializeApp(firebaseConfig);
+const db = initFirebase.firestore();
+
+export default db;
