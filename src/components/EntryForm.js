b86fd09aefab03e89e2b5ef8e7d99f03bd83bb1e
simgish <ryandoughty@gmail.com>
2021-07-15 14:42:59 -0400

    init image uploads

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -18,7 +18,26 @@ const EntryForm = ({ addNewEntry, addNewCategory, categories }) => {
     for (let i = 0; i < selectedImages.length; i++) {
       imageNames.push(selectedImages[i].name);
       imageSrcs.push(URL.createObjectURL(selectedImages[i]));
+      console.log('filename: ', selectedImages[i].name);
+      const uploadTask = storage.ref(`/images/${selectedImages[i].name}`).put(selectedImages[i]);
+      uploadTask.on('state_changed',
+        (snapShot) => {
+          //takes a snap shot of the process as it is happening
+          console.log(snapShot)
+        }, (err) => {
+          //catches the errors
+          console.log(err)
+        }, () => {
+          // gets the functions from storage refences the image storage in firebase by the children
+          // gets the download url then sets the image from firebase as the value for the imgUrl key:
+          storage.ref('images').child(selectedImages[i].name).getDownloadURL()
+            .then(fireBaseUrl => {
+              // setImageAsUrl(prevObject => ({ ...prevObject, imgUrl: fireBaseUrl }))
+              console.log(fireBaseUrl);
+            })
+        })
     }
+    return;
 
     const date = new Date();
     const entry = {
@@ -101,9 +120,9 @@ const EntryForm = ({ addNewEntry, addNewCategory, categories }) => {
           })}
         </ul>
       </form >
-      
-      { renderAddCategoryModal() }
-      
+
+      {renderAddCategoryModal()}
+
     </div>
   )
 }
