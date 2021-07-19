89da901dd82fdb8ef42bda16d4f0590e45d72788
simgish <ryandoughty@gmail.com>
2021-07-18 20:56:10 -0400

    emoticons and styling

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGrin, faFrown } from '@fortawesome/free-solid-svg-icons'
 
 const EntryList = ({ entries }) => {
 
@@ -8,13 +10,22 @@ const EntryList = ({ entries }) => {
     // let entryToEdit = entries.find((e) => e.id === id);
   }
 
+  const renderEmoticon = (emoticon) => {
+    if (emoticon === 'happy') {
+      return <FontAwesomeIcon icon={faGrin} />
+    } else if (emoticon === 'sad') {
+      return <FontAwesomeIcon icon={faFrown} />
+    }
+  }
+
   return (
     <div className="entry-list-container">
       <ul>
         {entries.map(entry => {
           return (
-            <li key={entry.id} onClick={() => updateEntry(entry.id)}>
+            <li key={entry.id} onClick={() => updateEntry(entry.id)} className="happy">
               <Link to={`/edit-entry/${entry.id}`}>
+                <span className="list-emoticon">{renderEmoticon('happy')}</span>
                 <span>{entry.title} - {entry.images.length} photos</span>
                 <span className="created-date">{format(entry.createdAt.toDate(), 'iii, LLL Mo, yy')}</span>
               </Link>
