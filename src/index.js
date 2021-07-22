61e72b02f6e6d027107676bbcab34bdc704d2b08
simgish <ryandoughty@gmail.com>
2021-07-21 22:47:43 -0400

    started removing redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import EntryReducer from './reducers/EntryReducer';
 import './index.css';
 import App from './App';
 
-const store = createStore(
-  EntryReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-store.subscribe(() => console.log(store.getState()));
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
