09ccbd42b9eaf50d05fe1c59b7c888f658041f7a
simgish <ryandoughty@gmail.com>
2021-07-24 15:41:22 -0400

    entry list emotion colors

--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFrown, faSmile } from '@fortawesome/free-solid-svg-icons'
+import { faSmile, faFrown, faAngry } from '@fortawesome/free-solid-svg-icons'
 
 const EntryList = ({ entries }) => {
 
@@ -15,6 +15,10 @@ const EntryList = ({ entries }) => {
       return <FontAwesomeIcon icon={faSmile} />
     } else if (emotion === 'sad') {
       return <FontAwesomeIcon icon={faFrown} />
+    } else if (emotion === 'neutral') {
+      return <FontAwesomeIcon icon={faSmile} />
+    } else if (emotion === 'angry') {
+      return <FontAwesomeIcon icon={faAngry} />
     }
   }
 
@@ -23,10 +27,10 @@ const EntryList = ({ entries }) => {
       <ul>
         {entries.map(entry => {
           return (
-            <li key={entry.id} onClick={() => updateEntry(entry.id)} className="happy">
+            <li key={entry.id} onClick={() => updateEntry(entry.id)} className={entry.emotion}>
               <Link to={`/edit-entry/${entry.id}`}>
                 <span className="left-group">
-                  <span className="list-emoticon">{renderEmoticon('happy')}</span>
+                  <span className="list-emoticon">{renderEmoticon(entry.emotion)}</span>
                   <span>{entry.title} - {entry.images.length} photos</span>
                 </span>
                 <span className="right-group">
